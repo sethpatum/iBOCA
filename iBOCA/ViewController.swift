0f7236337822cbd579a16d6f09f17ff9bd28f2c3
Seth <sethpatum@gmail.com>
2016-08-01 09:12:01 -0400

    Initial Commit

---
+++ iBOCA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iBOCA
+//
+//  Created by Seth Amarasinghe on 8/1/16.
+//  Copyright © 2016 sunspot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
