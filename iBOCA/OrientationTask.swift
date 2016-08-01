1467f466dc4e3f5dc892e434198c850cdc4ba291
17lime <ellisonlim99@gmail.com>
2016-08-01 15:19:10 -0400

    Added New Pickerviews and Text Fields

--- iBOCA/OrientationTask.swift
+++ iBOCA/OrientationTask.swift
@@ -11,12 +11,39 @@ import MessageUI
 
 var firstTimeThrough = true
 
-
+var Season : String?
+var State : String?
+var Town : String?
+var Address : String?
+var Date : String?
 class OrientationTask:  ViewController {
     
 
-
+    @IBOutlet weak var SeasonPicker: UIPickerView!
+    let seasonData = ["Spring", "Summer", "Fall", "Winter", "Don't know"]
  
+    @IBOutlet weak var StatePicker: UIPickerView!
+    let stateData = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia","Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
+    
+    @IBAction func updateDate(sender: AnyObject) {
+        let d:UIDatePicker = sender as! UIDatePicker
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "y-MM-dd"
+        Date = formatter.stringFromDate(d.date)
+    }
+    
+    @IBOutlet weak var addressField: UITextField!
+    @IBAction func updateAddress(sender: AnyObject) {
+        Address = addressField.text
+    }
+    
+    @IBOutlet weak var townField: UITextField!
+    @IBAction func updateTown(sender: AnyObject) {
+         Town = townField.text
+        
+        
+    }
+    
     override func viewDidLoad() {
     super.viewDidLoad()
     
