0ccc9188b0393c4f3a434663ed6422fb91d3064f
17lime <ellisonlim99@gmail.com>
2016-08-01 11:17:06 -0400

    Added Orientation Task View controller and swift file

---
+++ iBOCA/OrientationTask.swift
@@ -0,0 +1,44 @@
+//
+//  OrientationTask.swift
+//  iBOCA
+//
+//  Created by Ellison Lim on 8/1/16.
+//  Copyright © 2016 sunspot. All rights reserved.
+//
+
+import UIKit
+import MessageUI
+
+var firstTimeThrough = true
+
+
+class OrientationTask:  ViewController {
+    
+
+
+ 
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
