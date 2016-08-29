6bfc2415b24a0da542404dabe8ca57a4465c330a
Seth <sethpatum@gmail.com>
2016-08-29 11:36:21 -0400

    puuuuh

---
+++ iBOCA/3D Figure Copy.swift
@@ -0,0 +1,13 @@
+//
+//  3D Figure Copy.swift
+//  iBOCA
+//
+//  Created by Seth Amarasinghe on 8/29/16.
+//  Copyright © 2016 sunspot. All rights reserved.
+//
+
+import Cocoa
+
+class _D_Figure_Copy: NSObject {
+
+}
