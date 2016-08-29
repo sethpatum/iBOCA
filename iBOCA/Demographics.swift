59d5eb1a9b07033df5ec4bd3a8f46d666ce5eb2e
17lime <ellisonlim99@gmail.com>
2016-08-29 12:22:29 -0400

    Added Demographics

---
+++ iBOCA/Demographics.swift
@@ -0,0 +1,25 @@
+//
+//  Demographics.swift
+//  iBOCA
+//
+//  Created by Ellison Lim on 8/29/16.
+//  Copyright © 2016 sunspot. All rights reserved.
+//
+
+import UIKit
+
+class Demographics: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+       
+    }
+    
+
+}
