53813169e6841a7cf78aeb1a77493ec83690732c
Saman Amarasinghe <saman@lcs.mit.edu>
2017-06-16 20:12:00 -0400

    before start...

--- iBOCA/CatsAndDogsViewController.swift
+++ iBOCA/CatsAndDogsViewController.swift
@@ -529,11 +529,13 @@ class CatsAndDogsViewController: ViewController {
         incorrectRandom.append(otherCount)
         
         next()
-        
     }
-    //               0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
+    
+    
+    //  level        0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
     let levelcats = [0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 4, 4, 4, 4, 4, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
     let leveldogs = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 2, 2, 2, 2, 2, 4, 4, 4, 4, 4]
+    
     //user completed sequence; reset repeats, increase numplaces so 1 more button lights up
     func next(){
         
